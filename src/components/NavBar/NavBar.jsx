c00e3ab3c582dabaf3fcef284adb75e3e706e07c
Roberto Sanchez <rsanchez92@live.com>
2021-03-25 16:43:38 -0700

    Converted component to a container and added style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import './NavBar.css';
+import React, {Component} from 'react';
 import navLogo from '../../assets/navLogo.png'
 import {navLink} from 'react-router-dom';
+import styled from 'styled-components';
+class NavBar extends Component {
+    state={
 
-function NavBar(){
-    return <div className="navBar">
-        <img className="navLogo" src={navLogo} />
-        
-    </div>
+    }
+    
+    componentDidMount(){
+        // Need to find another place to have this at
+        const Nav = styled.div`
+        width: 100vw;
+        background-color: rgb(19,25,33);
+        display: inline-block;
+        `
+        const Logo = styled.img`
+            height: 50px;
+            display: inline-block;
+        `
+        const navItem = styled.a`
+            display: inline-block;
+        `
+    }
+    render(){
+        return(<div>
+            <Nav>
+                <Logo src={navLogo}/>
+                <navItem src="/">Link</navItem>
+            </Nav>
+        </div>)
+    }
 }
 export default NavBar;
