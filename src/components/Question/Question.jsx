8c385a097113138588eb91994b413c6a274706a9
Matt Freshwaters <mattfreshy@gmail.com>
2021-03-29 16:37:41 -0400

    more styling for component

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -22,44 +22,6 @@ class Question extends React.Component {
     }
   }
 
-  //   render() {
-  //     const questionList = this.state.questions.map((question) => {
-  //       const answers = question.answers.map((answer) => {
-  //         return (
-  //           <div class="question">
-  //             <div class="voteBox">
-  //               <div class="upVoteBtn" alt="upVoteDiv" onClick={addVote}></div>
-  //               <p class="x"> {question.votes} <br /> votes{" "} </p>
-  //               <div class="downVoteBtn" alt="downVoteDiv" onClick={removeVote}            ></div>
-  //             </div>
-  //             <div class="qNaBox">
-  //               <div class="questionHorizontal">
-  //                 <div id="qNaTitle">Question:</div>
-  //                 <div id="question">{question.question}</div>
-  //               </div>
-  //               <div class="answerHorizontal">
-  //                 <div id="answerTitle">Answer:</div>
-  //                 <div id="answer">{answer.answer}</div>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         );
-  //       });
-  //       function addVote(question) {
-  //         console.log("Adding a question");
-  //       }
-
-  //       function removeVote() {
-  //         console.log("Removing vote");
-  //       }
-
-  //       return <div>{answers}</div>;
-  //     });
-
-  //     return <div class="row col-12">{questionList}</div>;
-  //   }
-  // }
-
   render() {
     function addVote(question) {
       console.log("Adding a question");
@@ -69,14 +31,7 @@ class Question extends React.Component {
       console.log("Removing vote");
     }
     const questionList = this.state.questions.map((question) => {
-      console.log("Grabbing a question: ", question);
       const answersForQuestion = question.answers.map((answer) => {
-        console.log(
-          "Grabbing answer with id",
-          answer.answerId,
-          "for question",
-          question.questionId
-        );
         return <div class="answer">{answer.answer}</div>;
       });
       return (
@@ -99,7 +54,7 @@ class Question extends React.Component {
             </div>
             <div class="answerHorizontal">
               <div id="answerTitle">Answers:</div>
-              <div class="row col-12">{answersForQuestion}</div>
+              <div id="answers">{answersForQuestion}</div>
             </div>
           </div>
         </div>
@@ -111,7 +66,3 @@ class Question extends React.Component {
 }
 
 export default Question;
-
-//
-//<input type ="submit" class ="upVoteBtn" value="like" alt="upVote" onClick={addVote}></input>
-//<input type ="submit" class="downVoteBtn" value="dislike" alt="downVote" onClick={removeVote}></input>
