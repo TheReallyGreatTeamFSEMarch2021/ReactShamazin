196820855e8195566d8dac7ef17cfe362c253949
Tim <timbo.luu@gmail.com>
2021-03-30 16:44:35 -0400

    add additional css styling for related items

--- src/components/RelatedBoughtItemsComponent/RelatedItemDisplayComponent.jsx
+++ src/components/RelatedBoughtItemsComponent/RelatedItemDisplayComponent.jsx
@@ -1,13 +1,17 @@
 const RelatedItemDisplayComponent = ({item}) => {
+    const { name, primeEligible } = item;
     const photoURL = item.productPhotos.length > 0 ? item.productPhotos[0].photoURL : "";
     const price = (item.price).toFixed(2);
     
     return (
         <div>
-            <img src={photoURL} alt={item.name}/>
-            <div>{item.name}</div>
-            <div>{`$${price}`}</div>
-            <div>{item.primeEligible ? "prime" : ""}</div>
+            <img src={photoURL} alt={name}/>
+            <div className="RelatedItemName">{name}</div>
+            <div className="RelatedItemPrice">{`$${price}`}</div>
+            <img 
+                className="RelatedItemPrimeEligibility" 
+                src={primeEligible ? process.env.PUBLIC_URL + "/prime_logo.png" : ""} 
+            />
         </div>
     )
 };
