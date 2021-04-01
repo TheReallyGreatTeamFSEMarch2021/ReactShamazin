ba603706908081d9e3d6e65161d82b0519063dff
timluu <73354960+timluu@users.noreply.github.com>
2021-04-01 15:23:10 -0400

    add further styling to related items component (#29)
    * add arrow images
    
    * integrate arrow assets into navigation
    
    * style and refactor container of related items

--- src/components/RelatedBoughtItemsComponent/RelatedItemDisplayComponent.jsx
+++ src/components/RelatedBoughtItemsComponent/RelatedItemDisplayComponent.jsx
@@ -1,17 +1,23 @@
-const RelatedItemDisplayComponent = ({item}) => {
-    const { name, primeEligible } = item;
-    const photoURL = item.productPhotos.length > 0 ? item.productPhotos[0].photoURL : "";
-    const price = (item.price).toFixed(2);
-    
+const RelatedItemDisplayComponent = ({ relatedItems }) => {
     return (
-        <div>
-            <img src={photoURL} alt={name}/>
-            <div className="RelatedItemName">{name}</div>
-            <div className="RelatedItemPrice">{`$${price}`}</div>
-            <img 
-                className="RelatedItemPrimeEligibility" 
-                src={primeEligible ? process.env.PUBLIC_URL + "/prime_logo.png" : ""} 
-            />
+        <div className="RelatedItemDisplayContainer">
+            {relatedItems.map(item => {
+                const { name, primeEligible } = item;
+                const photoURL = item.productPhotos.length > 0 ? item.productPhotos[0].photoURL : "";
+                const price = (item.price).toFixed(2);
+                
+                return (
+                    <div className="RelatedItem">
+                        <img src={photoURL} alt={name}/>
+                        <div className="RelatedItemName">{name}</div>
+                        <div className="RelatedItemPrice">{`$${price}`}</div>
+                        <img 
+                            className="RelatedItemPrimeEligibility" 
+                            src={primeEligible ? process.env.PUBLIC_URL + "/prime_logo.png" : ""} 
+                        />
+                    </div>
+                )
+            })}
         </div>
     )
 };
