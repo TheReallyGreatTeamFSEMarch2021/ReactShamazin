cd550962742165d17b4e5e8660164d2017d4fa8a
Fei Yang <feidyang@gmail.com>
2021-04-02 12:19:35 -0400

    Feature login page (#38)
    * aligning using css and added default product photo for all items
    
    * added default pic and further styling of nav
    
    * styled loginPage
    
    * removed grey line
    
    * just added margin for itemPageContainer
    
    Co-authored-by: Fei Yang <fyang@C02CMKHKML7H.fios-router.home>

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
+import '../../index.css';
+import './Login.css';
+import {Link} from 'react-router-dom';
 
 
 class Login extends React.Component{
@@ -17,9 +20,30 @@ class Login extends React.Component{
     
     render(){   
         return(
-            <div>
-               Login Page
-            </div>
+            <div class="loginPageDiv">
+                <div class="row col-12">
+                    <div class="col-4"></div>
+                    <div class="col-4 loginContent"> 
+                        <img class="shamazinLoginLogo" src="https://shamazin.s3.amazonaws.com/shamazinLogo.jpeg" alt="shamazin logo"/>
+                        <div class="SignInBox">
+                            <span class="signInHeader" >Sign-In</span><br/>
+                            <label class="emailOrPhoneNumText">Email or mobile phone number</label><br/>
+                            <input class="inputForSignIn" type="text"></input><br/>
+                            <button class="continueButton">Continue</button>
+                            <p class="privacySpan">By continuing, you agree to Amazon's Conditions of Use and Privacy notice.</p>
+                            
+                        </div>
+                        <div class="shamazinLoginLastDiv">
+                        New to Amazon?<br/>
+                            <button class="RegisterButton">
+                                <Link class="linkToRegister" to='/register' >Create your Amazon account</Link>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-4"></div>
+                </div>
+            </div>    
+           
         )
     }
 }
