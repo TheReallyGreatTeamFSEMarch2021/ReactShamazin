b3cd306673d17ad7b8a92f374e71973d24f884c3
Fei Yang <fyang@C02CMKHKML7H.fios-router.home>
2021-04-02 14:08:59 -0400

    deleted line

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,8 +9,7 @@ class Login extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            title: "LOGIN PAGE",
-            
+            title: "LOGIN PAGE"
         }   
     }
 
