14e31bcb461c076372a7c471b7ba139336aac8a9
Nathaniel Eveland <54957992+neveland98@users.noreply.github.com>
2021-04-02 10:45:32 -0500

    features component (item features like color, size etc, also has price and prime eligibility) (#37)
    * added features component that displays different properties of a product, plus the price
    also deleted old shamazin component because it was confusing
    
    * added features component that displays different properties of a product, pluse the price. also deleted the old shamazin component because it was confusing.
    
    * added rectangular prime logo, fixed src so the logo would actually show up, updated styling
    
    * merged in staging changes, updated titleComponent so it will display properly for all possible states

--- src/components/Title/titleComponent.jsx
+++ src/components/Title/titleComponent.jsx
@@ -4,6 +4,7 @@ import StarRating from '../ReviewStarRating/starRating';
 import ReviewPopup from './ReviewPopup/reviewPopup';
 import './titleComponent.css';
 import InfosComponent from '../Infos/Infos';
+import FeaturesComponent from './FeaturesComponent/featuresComponent';
 class TitleComponent extends React.Component {
     constructor(props) {
         super(props);
@@ -11,7 +12,8 @@ class TitleComponent extends React.Component {
             rating: 0,
             reviews: [],
             showPopup: false,
-            itemFamilyID: 0
+            itemFamilyID: 0,
+            item: this.props.item
         }
         this.showRatings=this.showRatings.bind(this);
     }
@@ -24,7 +26,8 @@ class TitleComponent extends React.Component {
                 })
             })
         this.setState({
-            rating: Math.round(this.props.rating)
+            rating: Math.round(this.props.rating),
+            item: this.props.item
         });
     }
     componentDidUpdate(prevProps,prevState) {
@@ -44,6 +47,11 @@ class TitleComponent extends React.Component {
                 itemFamilyID: this.props.itemFamilyID
             })
         }
+        if(this.props.item != prevProps.item) {
+            this.setState({
+                item: this.props.item
+            });
+        }
     }
     showRatings() {
         if(this.state.showPopup) this.setState({showPopup: false});
@@ -52,25 +60,61 @@ class TitleComponent extends React.Component {
     }
 
     render() {
-        if(this.state.showPopup) {
+        if(this.state.showPopup && this.state.item.primeEligible) {
             return(
                 <div className="col-itemInfo">
                     <div className="row col-12">
                         <h1>{this.props.item.name}</h1>
                         <StarRating starRating={this.state.rating}/>
-                        <h3 className="reviewPopup" onMouseLeave={()=>this.showRatings()}>{this.state.reviews.length} ratings
+                        <h3 className="reviewPopup" onMouseLeave={()=>this.showRatings()}>{this.state.reviews.length} ratings<img className="darr" src="/downarrow.png" alt=""/>
                             <ReviewPopup itemFamilyID={this.state.itemFamilyID}/>
                         </h3>
                         <h3>{this.state.reviews.length} answered questions</h3>
                     </div>
-                    <div className='row col-12'>
-                        <h1>Features Component</h1>
-                        <h3>Includes Price, Prime Logo</h3>
-                        <h3>Includes Diff Features</h3>
+                    <div className='row col-12'></div>
+                    <h3>Price: ${this.state.item.price}<img className="primeTitleLogo" src="/prime_logo_rect.png" alt=""/></h3>
+                    <FeaturesComponent properties={this.state.item.properties}/>
+                        <div className='row col-12'>
+                            <InfosComponent infos = {this.props.infos}/>
+                        </div>
+                </div>
+            );
+        }
+        else if(this.state.item.primeEligible) {
+            return(
+                <div className="col-itemInfo">
+                    <div className="row col-12">
+                        <h1>{this.props.item.name}</h1>
+                        <StarRating starRating={this.state.rating}/>
+                        <h3 className="reviewPopup" onMouseEnter={()=>this.showRatings()}>{this.state.reviews.length} ratings<img className="darr" src="/downarrow.png" alt=""/></h3>
+                        <h3>{this.state.reviews.length} answered questions</h3>
                     </div>
-                    <div className='row col-12'>
-                        <InfosComponent infos = {this.props.infos}/>
+                    <div className='row col-12'></div>
+                    <h3>Price: ${this.state.item.price}<img className="primeTitleLogo" src="/prime_logo_rect.png" alt=""/></h3>
+                    <FeaturesComponent properties={this.state.item.properties}/>
+                        <div className='row col-12'>
+                            <InfosComponent infos = {this.props.infos}/>
+                        </div>
+                </div>
+            );
+        }
+        else if(this.state.showPopup) {
+            return(
+                <div className="col-itemInfo">
+                    <div className="row col-12">
+                        <h1>{this.props.item.name}</h1>
+                        <StarRating starRating={this.state.rating}/>
+                        <h3 className="reviewPopup" onMouseLeave={()=>this.showRatings()}>{this.state.reviews.length} ratings<img className="darr" src="/downarrow.png" alt=""/>
+                            <ReviewPopup itemFamilyID={this.state.itemFamilyID}/>
+                        </h3>
+                        <h3>{this.state.reviews.length} answered questions</h3>
                     </div>
+                    <div className='row col-12'></div>
+                    <h3>Price: ${this.state.item.price}</h3>
+                    <FeaturesComponent properties={this.state.item.properties}/>
+                        <div className='row col-12'>
+                            <InfosComponent infos = {this.props.infos}/>
+                        </div>
                 </div>
             );
         }
@@ -80,21 +124,18 @@ class TitleComponent extends React.Component {
                     <div className="row col-12">
                         <h1>{this.props.item.name}</h1>
                         <StarRating starRating={this.state.rating}/>
-                        <h3 className="reviewPopup" onMouseEnter={()=>this.showRatings()}>{this.state.reviews.length} ratings</h3>
+                        <h3 className="reviewPopup" onMouseEnter={()=>this.showRatings()}>{this.state.reviews.length} ratings<img className="darr" src="/downarrow.png" alt=""/></h3>
                         <h3>{this.state.reviews.length} answered questions</h3>
                     </div>
-                    <div className='row col-12'>
-                        <h1>Features Component</h1>
-                        <h3>Includes Price, Prime Logo</h3>
-                        <h3>Includes Diff Features</h3>
-                    </div>
-                    <div className='row col-12'>
-                        <InfosComponent infos = {this.props.infos}/>
-                    </div>
+                    <div className='row col-12'></div>
+                    <h3>Price: ${this.state.item.price}</h3>
+                    <FeaturesComponent properties={this.state.item.properties}/>
+                        <div className='row col-12'>
+                            <InfosComponent infos = {this.props.infos}/>
+                        </div>
                 </div>
             );
-        }
-        
+        }  
     }
 
 }
